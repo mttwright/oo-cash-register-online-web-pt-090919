9bc92a7af2ee94f450ea4359948bf959b6a6f2ac
virtual-frame-7789 <mttwright@gmail.com>
2019-10-09 02:56:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
-
+class CashRegister
+  
+  attr_accessor :total
+  attr_reader :discount, :items
+  
+  def initialize(discount=0)
+    @items = []
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity=1)
+    counter = quantity
+    while counter >= 1 do
+      @items.push(title)
+      counter -= 1
+    end
+      @total = @total + (price * quantity)
+  end
+  
+  def apply_discount
+    if discount == 0
+      "There is no discount to apply."
+    else
+      @total = @total * (1.00 - (@discount.to_f / 100))
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+  
+    
+    
+end
